8618abe5a5c0cedf3e5f9f405af3f0b7cefbbf91
HeathK1iff <HeathK1iff@outlook.com>
2024-02-19 22:47:06 +0300

    Prepared structure projects of solution

---
+++ src/UserAuth/UserAuth.WebAPI/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddLogging();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
+app.Run();
